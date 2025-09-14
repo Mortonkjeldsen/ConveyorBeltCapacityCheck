e09de95f051d9f643be117661d4ef7cf64429b97
Morten Kjeldsen <morten.tilde@hotmail.com>
2025-09-14 15:32:20 +0200

    Mainfunctionworks

--- src/main.c
+++ src/main.c
@@ -5,8 +5,13 @@ double packageweight;
 const double maxmotorload = 5.6;
 
 int main(void) {
-    printf("How many motors are carrying the packages? ");
-    scanf("%d", &motorcount);
-    printf("How many kg of packages do we expect? ");
-    scanf("%lf", &packageweight);
+  printf("How many motors are carrying the packages? ");
+  scanf("%d", &motorcount);
+  printf("How many kg of packages do we expect? ");
+  scanf("%lf", &packageweight);
+
+  if (motorcount * maxmotorload >= packageweight)
+    printf("The conveyor belt can carry the packages.");
+  else
+    printf("The conveyor belt cannot carry the packages.");
 }
