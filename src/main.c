6ba53d903df5bfde41693be3d6fc1a1ef0828360
Morten Kjeldsen <morten.tilde@hotmail.com>
2025-09-14 15:57:49 +0200

    Whileloop

--- src/main.c
+++ src/main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
+int navigation = 1;
 int motorcount;
 double packageweight;
 const double maxmotorload = 5.6;
 
 int main(void) {
-  printf("How many motors are carrying the packages? ");
-  scanf("%d", &motorcount);
-  printf("How many kg of packages do we expect? ");
-  scanf("%lf", &packageweight);
+  while (navigation != 0) {
+    printf("How many motors are carrying the packages? ");
+    scanf("%d", &motorcount);
+    printf("How many kg of packages do we expect? ");
+    scanf("%lf", &packageweight);
 
-  if (motorcount * maxmotorload >= packageweight)
-    printf("The conveyor belt can carry the packages.");
-  else
-    printf("The conveyor belt cannot carry the packages.");
+    if (motorcount * maxmotorload >= packageweight)
+      printf("The conveyor belt can carry the packages. \n");
+    else
+      printf("The conveyor belt cannot carry the packages. \n");
+
+    printf("To exit type 0, to repeat type 1: ");
+    scanf("%d", &navigation);
+  }
 }
